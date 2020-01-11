39f13c9384a2c33838f5d39de17be263f2d96c19
Baptiste <baptistevallet@etu.uca.fr>
2020-01-11 18:58:50 +0100

    Oui! je gère les déplacements et quand on marche sur un interrupteur. Prochaine etape les caisses!

--- SokobanFonctionnement.c
+++ SokobanFonctionnement.c
@@ -3,6 +3,7 @@
 #include<SDL.h>
 #include<SDL_types.h>
 #include<time.h>
+#include<string.h>
 
 typedef struct position{
   int x;
@@ -21,7 +22,7 @@ SDL_Surface * ecran = NULL;
 
 //void gauche();
 SDL_Event pause();
-void trouvePerso(char ** TJeu,int taille,position * perso);
+void trouvePerso(char ** TJeu,int taille,position * perso,char* caseInit);
 void afficheTab2D(char ** tab, int taille);
 void deplacement(char** TJeu,int taille);
 
@@ -39,29 +40,30 @@ int main(){
     fprintf(stderr,"Erreur VideoMode : %s\n",SDL_GetError());
     exit(EXIT_FAILURE);}
 
-  char** TJeu =(char**) malloc(4*sizeof(char*));
-  TJeu[0]="    ";
-  TJeu[1]="    ";
-  TJeu[2]="    ";
-  TJeu[3]="   P";
+  char** TJeu =(char**) malloc(taille*sizeof(char*));
+  TJeu[0]=   (char*) malloc((taille+1)*sizeof(char));
+  TJeu[1]=  (char*) malloc((taille+1)*sizeof(char));
+  TJeu[2]=   (char*) malloc((taille+1)*sizeof(char));
+  TJeu[3]=  (char*) malloc((taille+1)*sizeof(char));
+  int i,j;
+  for(i = 0; i < taille; i++)
+  {
+    for(j = 0; j < taille; j++)
+    {
+       TJeu[i][j]=' ';
+    }
+  }
+  TJeu[3][3]='P';
+  TJeu[2][2]='I';
 
-  position perso;
-  perso.x=0;
-  perso.y=0;
-  printf("avant");
-  pause();
-  printf("apres");
   
-  //  trouvePerso(TJeu,4,&perso);
-  //  afficheTab2D(TJeu,4);
-  // gauche();
-  deplacement(TJeu,4);
+  deplacement(TJeu,taille);
   SDL_Quit();
   free(TJeu);
   
   return 0;}
 
-void trouvePerso(char ** TJeu,int taille,position * perso){
+void trouvePerso(char ** TJeu,int taille,position * perso,char* caseInit){
   // position perso;
   perso->x=0;
   perso->y=0;
@@ -77,6 +79,7 @@ void trouvePerso(char ** TJeu,int taille,position * perso){
 	}
       }
   }
+  caseInit=&TJeu[perso->y][perso->x];
   //printf("position=%d %d\n",perso->x,perso->y);
 }
     
@@ -96,23 +99,7 @@ void afficheTab2D(char ** tab, int taille)
   printf("\n");
 }
 
-/*void gauche(){
-  SDL_Event event;
-  int cont =1;
-  while(cont){
-    printf("a");
-    SDL_WaitEvent(&event);
-    switch(event.type){
-    case SDL_QUIT:
-      cont = 0;
-      break;
-    case SDL_KEYDOWN:
-      cont = 0;
-      break;
-    default:
-      cont=1;
-    }}}
-*/
+
 	
 SDL_Event pause(){
   // printf("oijio");
@@ -136,54 +123,101 @@ SDL_Event pause(){
 
 void deplacement(char** TJeu,int taille){
   int cont=1 ;
+  char* change= (char*) malloc((taille+1)*sizeof(char));
   SDL_Event event ;
   position perso;
-  trouvePerso(TJeu,taille,&perso);
+  position posPrev;
+  char nouvCase;
+  char exCase;
+  
+  trouvePerso(TJeu,taille,&perso,&nouvCase);
+  afficheTab2D(TJeu,taille);
   while(cont){
+    posPrev= perso;
+    exCase=nouvCase;
     printf("position=%d %d\n",perso.x,perso.y);
+    printf("position=%d %d\n",posPrev.x,posPrev.y);
     event= pause();
-    afficheTab2D(TJeu,taille);
-    //    SDL_WaitEvent(&event) ;
+    
     switch(event.type){
     case SDL_QUIT:
       cont=0;
-      // printf("addddddddddddddddddddddddddddddddddd");
     case SDL_KEYDOWN:
       switch(event.key.keysym.sym){
       case SDLK_UP:
-	if(perso.y>0 && ( TJeu[perso.y -1][perso.x] == 'I' || TJeu[perso.y-1][perso.x] == ' ')){
-	  // TJeu[perso.y][perso.x]=' ';
+	if(perso.y>0){
+	  if( TJeu[perso.y -1][perso.x] == 'I'){
+	    perso.y--;
+	    nouvCase='p';
+	  }
+	  else if(TJeu[perso.y-1][perso.x] == ' '){
 	  
 	  perso.y--;
-	  //TJeu[perso.y][perso.x]='P';
-	  //printf("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
-	  
+	  nouvCase='P';
+	  }
 	}
-	//dessine(ecran) ;
+      
 	break ;
       case SDLK_DOWN:
-	if(perso.y<taille-1 && ( TJeu[perso.y +1][perso.x] == 'I' || TJeu[perso.y+1][perso.x] == ' ')){
-	  perso.y++;
+	if(perso.y<taille-1){
+	  if( TJeu[perso.y +1][perso.x] == 'I'){
+	    perso.y++;
+	    nouvCase='p';}
+	  else if(TJeu[perso.y+1][perso.x] == ' '){
+	    perso.y++;
+	    nouvCase='P';
+	  }
 	}
-	//dessine(ecran) ;
 	break ;
-
+	
       case SDLK_LEFT :
-	if(perso.x>0 && ( TJeu[perso.y][perso.x -1] == 'I' || TJeu[perso.y][perso.x-1] == ' ')){
+	if(perso.x>0){
+	  if( TJeu[perso.y][perso.x -1] == 'I'){
+	    
+	    perso.x--;
+	    nouvCase='p';}
+	  
+	  else if(TJeu[perso.y][perso.x-1] == ' '){
 	  perso.x--;
+	    nouvCase='P';
+	  }
 	}
 	//dessine(ecran);
 	break ;
       case SDLK_RIGHT:
-	if(perso.x<taille-1 && ( TJeu[perso.y][perso.x +1] == 'I' || TJeu[perso.y][perso.x+1] == ' ')){
-	  perso.x++;
+	if(perso.x<taille-1){
+	  if( TJeu[perso.y][perso.x +1] == 'I'){
+	    perso.x++;
+	    nouvCase='p';}
+	  else if(TJeu[perso.y][perso.x+1] == ' '){
+	    perso.x++;
+	    nouvCase='P';
+	  }
 	}
-	//dessine(ecran) ;
 	break ;
-      case 'q':cont=0 ;
+      case 'q':
+	cont=0 ;
 	break ;
       }
     }
+
+    if(exCase =='P'){
+      exCase=' ';}
+    else{
+      exCase='I';}
+    
+    
+    strcpy(change,TJeu[posPrev.y]);
+    change[posPrev.x]=exCase;  
+    strcpy(TJeu[posPrev.y],change);
+
+    
+    strcpy(change,TJeu[perso.y]);
+    change[perso.x]=nouvCase;  
+    strcpy(TJeu[perso.y],change);
+
+    
+    afficheTab2D(TJeu,taille);
   }
 
 }
